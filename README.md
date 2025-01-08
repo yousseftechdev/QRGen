5550b75f95ccaf45b8f39aaa389e35b1fbaf20a6
yousseftechdev <youssefmostafa200895@gmail.com>
2025-01-08 11:58:33 +0200

    updated some stuff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # QRGen
 ## Minimal QR code generator
+
+## Changelog:
+- Added: Customization options
+- Added: Download button
+- Added: Live preview
+- Changed: UI Lauout
--- js/script.js
+++ js/script.js
@@ -1,12 +1,47 @@
 const input = document.querySelector("input");
 const output = document.querySelector("img#output");
+const colorInput = document.querySelector("#color");
+const sizeInput = document.querySelector("#size");
+const downloadButton = document.querySelector("#download");
+const bgColorInput = document.querySelector("#bgColor");
 
-document.querySelector("button").addEventListener("click", function() {
-  QRCode.toDataURL(input.value, function(err, url) {
+
+// document.querySelector("button").addEventListener("click", function() {
+//   const options = {
+//     color: {
+//       dark: colorInput.value,
+//       light: "#FFFFFF"
+//     },
+//     width: parseInt(sizeInput.value)
+//   };
+
+//   QRCode.toDataURL(input.value, options, function(err, url) {
+//     if (err) {
+//       output.src = "";
+//       return console.error(err);
+//     }
+//     output.src = url;
+//     downloadButton.href = url;
+//     downloadButton.style.display = "block";
+//   });
+// });
+
+input.addEventListener("keyup", function() {
+  const options = {
+    color: {
+      dark: colorInput.value,
+      light: bgColorInput.value
+    },
+    width: parseInt(sizeInput.value)
+  };
+
+  QRCode.toDataURL(input.value, options, function(err, url) {
     if (err) {
       output.src = "";
       return console.error(err);
     }
     output.src = url;
+    downloadButton.href = url;
+    downloadButton.style.display = "block";
   });
 });
